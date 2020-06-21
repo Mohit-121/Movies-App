cb478dcdac823889f048716be0113e4d4c1c451e
Mohit <mc27768@gmail.com>
2020-06-21 16:18:50 +0530

    added combineReducers() method

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
+import {combineReducers} from 'redux';
 import {ADD_MOVIES,ADD_TO_FAVOURITE,REMOVE_FROM_FAVOURITE, SET_SHOW_FAVOURITES} from '../actions/index';
 
 const initialMoviesState = {
@@ -42,13 +43,18 @@ export function search(state=initialSearchState,action){
     return state;
 }
 
-const initialRootState = {
-    movies:initialMoviesState,
-    search:initialSearchState
-};
-export default function rootReducer(state=initialRootState,action){
-    return {
-        movies: movies(state.movies,action),
-        search: search(state.search,action)
-    }
-}
+// const initialRootState = {
+//     movies:initialMoviesState,
+//     search:initialSearchState
+// };
+// export default function rootReducer(state=initialRootState,action){
+//     return {
+//         movies: movies(state.movies,action),
+//         search: search(state.search,action)
+//     }
+// }
+
+export default combineReducers({
+    movies,
+    search
+});
