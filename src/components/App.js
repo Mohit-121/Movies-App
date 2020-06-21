158e9de34390534c3f38140c1fefa12a1267adc5
Mohit <mc27768@gmail.com>
2020-06-21 16:11:12 +0530

    added search reducer and a root reducer to manage both movies and search reducer

--- src/components/App.js
+++ src/components/App.js
@@ -19,8 +19,8 @@ class App extends React.Component{
   }
 
   isMovieFavourite = (movie) =>{
-    const {favourites} = this.props.store.getState();
-    const index = favourites.indexOf(movie);
+    const {movies} = this.props.store.getState();
+    const index = movies.favourites.indexOf(movie);
     if(index===-1) return false;
     return true;
   }
@@ -31,7 +31,8 @@ class App extends React.Component{
   }
 
   render(){
-    const {list,favourites,showFavourites}=this.props.store.getState();
+    const {movies} = this.props.store.getState();
+    const {list,favourites,showFavourites}=movies;
     console.log(this.props.store.getState());
     const displayMovies = showFavourites?favourites:list;
     return (
